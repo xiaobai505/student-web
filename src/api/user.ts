ea1fd4919b3186957b1c3c6250d682599f00f048
t-guoyu.dong <407479757@qq.com>
2023-07-12 17:38:25 +0800

    perf: 完善接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -46,7 +46,9 @@ export const getLogin = (data?: object) => {
 
 /** 刷新token */
 export const refreshTokenApi = (data?: object) => {
-  return http.request<RefreshTokenResult>("post", "/refreshToken", { data });
+  return http.request<RefreshTokenResult>("post", "/api/auth/refreshToken", {
+    data
+  });
 };
 
 // 获取用户
