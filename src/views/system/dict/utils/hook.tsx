906f9e97d109a3ee6d8916e536aee30ced6469e4
t-guoyu.dong <t-guoyu.dong@pcitc.com>
2023-09-05 22:17:53 +0800

    perf: 打包报错

--- src/views/system/dict/utils/hook.tsx
+++ src/views/system/dict/utils/hook.tsx
@@ -1,13 +1,11 @@
 import dayjs from "dayjs";
-import editForm from "../form.vue";
 import { handleTree } from "@/utils/tree";
 import { message } from "@/utils/message";
-import { getDeptList, saveOrUpdateDept } from "@/api/dept";
+import { getDeptList } from "@/api/dept";
 import { usePublicHooks } from "../../hooks";
-import { addDialog } from "@/components/ReDialog";
-import { reactive, ref, onMounted, h } from "vue";
+import { reactive, ref, onMounted } from "vue";
 import { type FormItemProps } from "../utils/types";
-import { cloneDeep, isAllEmpty } from "@pureadmin/utils";
+import { isAllEmpty } from "@pureadmin/utils";
 
 export function useDept() {
   const form = reactive({
@@ -15,7 +13,6 @@ export function useDept() {
     status: null
   });
 
-  const formRef = ref();
   const dataList = ref([]);
   const loading = ref(true);
   const { tagStyle } = usePublicHooks();
@@ -90,70 +87,8 @@ export function useDept() {
     }, 500);
   }
 
-  function formatHigherDeptOptions(treeList) {
-    // 根据返回数据的status字段值判断追加是否禁用disabled字段，返回处理后的树结构，用于上级部门级联选择器的展示（实际开发中也是如此，不可能前端需要的每个字段后端都会返回，这时需要前端自行根据后端返回的某些字段做逻辑处理）
-    if (!treeList || !treeList.length) return;
-    const newTreeList = [];
-    for (let i = 0; i < treeList.length; i++) {
-      treeList[i].disabled = treeList[i].status === 0 ? true : false;
-      formatHigherDeptOptions(treeList[i].children);
-      newTreeList.push(treeList[i]);
-    }
-    return newTreeList;
-  }
-
   function openDialog(title = "新增", row?: FormItemProps) {
-    addDialog({
-      title: `${title}部门`,
-      props: {
-        formInline: {
-          id: row?.id ?? 0,
-          higherDeptOptions: formatHigherDeptOptions(cloneDeep(dataList.value)),
-          parentId: row?.parentId ?? 0,
-          name: row?.name ?? "",
-          leaderuserid: row?.leaderuserid ?? "",
-          phone: row?.phone ?? "",
-          email: row?.email ?? "",
-          sort: row?.sort ?? 0,
-          status: row?.status ?? 1,
-          remark: row?.remark ?? ""
-        }
-      },
-      width: "40%",
-      draggable: true,
-      fullscreenIcon: true,
-      closeOnClickModal: false,
-      contentRenderer: () => h(editForm, { ref: formRef }),
-      beforeSure: (done, { options }) => {
-        const FormRef = formRef.value.getRef();
-        const curData = options.props.formInline as FormItemProps;
-        function chores() {
-          saveOrUpdateDept(curData)
-            .then(() => {
-              message(`您${title}了部门名称为${curData.name}的这条数据`, {
-                type: "success"
-              });
-            })
-            .finally(() => {
-              done(); // 关闭弹框
-              onSearch(); // 刷新表格数据
-            });
-        }
-        FormRef.validate(valid => {
-          if (valid) {
-            console.log("curData", curData);
-            // 表单规则校验通过
-            if (title === "新增") {
-              // 实际开发先调用新增接口，再进行下面操作
-              chores();
-            } else {
-              // 实际开发先调用编辑接口，再进行下面操作
-              chores();
-            }
-          }
-        });
-      }
-    });
+    console.log(title + "---" + row);
   }
 
   function handleDelete(row) {
