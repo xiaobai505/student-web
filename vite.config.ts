a81c73d914441161d82f08c763bf2e537328f68d
t-guoyu.dong <t-guoyu.dong@pcitc.com>
2023-09-07 21:00:57 +0800

    perf: 更新请求路径

--- vite.config.ts
+++ vite.config.ts
@@ -47,8 +47,8 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
         "^/api/.*": {
           // 这里填写后端地址
           target: "http://127.0.0.1:8081",
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, "")
+          changeOrigin: true
+          // rewrite: path => path.replace(/^\/api/, "")
         }
       }
     },
